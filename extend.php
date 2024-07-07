60ec4dece844e8de8aecc9ddf2057895c5ce4653
Googol <googology+github@outlook.com>
2024-07-07 13:52:44 +0800

    Delete the needn't extension

--- extend.php
+++ extend.php
@@ -4,9 +4,6 @@ use Flarum\Extend;
 use s9e\TextFormatter\Configurator;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->css(__DIR__ . '/less/forum.less'),
-
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
